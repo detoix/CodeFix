f69e4c4f7fd795d22188e0ce51f26f13cb7fcc3a
kdeptula93 <kdeptula93@outlook.com>
2018-04-18 19:24:21 +0200

    Initial commit

--- CodeFix/CodeFix.Test/CodeFixUnitTests.cs
+++ CodeFix/CodeFix.Test/CodeFixUnitTests.cs
@@ -11,72 +11,36 @@ namespace CodeFix.Test
     [TestClass]
     public class UnitTest : CodeFixVerifier
     {
-
-        //No diagnostics expected to show up
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodX()
         {
-            var test = @"";
-
-            VerifyCSharpDiagnostic(test);
-        }
-
-        //Diagnostic and CodeFix both triggered and checked for
-        [TestMethod]
-        public void TestMethod2()
-        {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TypeName
-        {   
-        }
-    }";
-            var expected = new DiagnosticResult
+            var source = @"
+            class Class
             {
-                Id = "CodeFix",
-                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-                Severity = DiagnosticSeverity.Warning,
-                Locations =
-                    new[] {
-                            new DiagnosticResultLocation("Test0.cs", 11, 15)
-                        }
-            };
+                public Class(string arg)
+                {
 
-            VerifyCSharpDiagnostic(test, expected);
+                }
+            }";
 
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
+            var expected = @"
+            class Class
+            {
+    public string Arg { get; }
 
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-            VerifyCSharpFix(test, fixtest);
+    public Class(string arg)
+                {
+
+                }
+            }";
+            VerifyCSharpFix(source.Replace("\n", Environment.NewLine),
+                expected.Replace("\n", Environment.NewLine));
         }
 
         protected override CodeFixProvider GetCSharpCodeFixProvider()
-        {
-            return new CodeFixCodeFixProvider();
-        }
+            => new CodeFixCodeFixProvider();
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
-        {
-            return new CodeFixAnalyzer();
-        }
+            => new CodeFixAnalyzer();
     }
 }
--- CodeFix/CodeFix/CodeFixCodeFixProvider.cs
+++ CodeFix/CodeFix/CodeFixCodeFixProvider.cs
@@ -10,64 +10,66 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
 
 namespace CodeFix
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(CodeFixCodeFixProvider)), Shared]
     public class CodeFixCodeFixProvider : CodeFixProvider
     {
-        private const string title = "Make uppercase";
-
         public sealed override ImmutableArray<string> FixableDiagnosticIds
-        {
-            get { return ImmutableArray.Create(CodeFixAnalyzer.DiagnosticId); }
-        }
+            => ImmutableArray.Create(CodeFixAnalyzer.DiagnosticId);
 
         public sealed override FixAllProvider GetFixAllProvider()
-        {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
-            return WellKnownFixAllProviders.BatchFixer;
-        }
+            => WellKnownFixAllProviders.BatchFixer;
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var root = await context
+                .Document
+                .GetSyntaxRootAsync(context.CancellationToken)
+                .ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent;
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
-
-            // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
                 CodeAction.Create(
-                    title: title,
-                    createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c), 
-                    equivalenceKey: title),
+                    "Update properties",
+                    e => UpdateProperties(context.Document, declaration, e)),
                 diagnostic);
         }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private async Task<Document> UpdateProperties(Document document, SyntaxNode constructor, CancellationToken cancellationToken)
         {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
+            if (constructor is ConstructorDeclarationSyntax declaration)
+            {
+                var syntaxTree = await document
+                    .GetSyntaxTreeAsync(cancellationToken);
+                var newSyntaxTree = syntaxTree
+                    .GetRoot()
+                    .InsertNodesBefore(
+                        constructor,
+                        declaration.ParameterList.Parameters.Select(e => e.ConvertToProperty()));
 
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+                document = document.WithSyntaxRoot(newSyntaxTree);
+            }
 
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
-
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
+            return document;
         }
+    }
+
+    public static class Helpers
+    {
+        public static PropertyDeclarationSyntax ConvertToProperty(this ParameterSyntax parameter)
+            => SyntaxFactory.PropertyDeclaration(parameter.Type, parameter.Identifier.Text.CapitalizeFirstLetter())
+                .AddModifiers(
+                    SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                .AddAccessorListAccessors(
+                    SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                        .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)));
+
+        public static string CapitalizeFirstLetter(this string s)
+            => char.ToUpper(s[0]) + s.Substring(1);
     }
 }
